9f002c77e0304a53adce4cd5b7958a51698bf68e
sajinkpk980 <sajinkpk980@gmail.com>
2023-05-04 10:29:26 +0530

    First commit -Engg 1

---
+++ src/test/java/com/ecommerce/flipkartpractise/GitHubPractice.java
@@ -0,0 +1,10 @@
+package com.ecommerce.flipkartpractise;
+
+import org.testng.annotations.Test;
+
+public class GitHubPractice {
+@Test
+public void gitHub() {
+	System.out.println("this is a practice for github push");
+}
+}
